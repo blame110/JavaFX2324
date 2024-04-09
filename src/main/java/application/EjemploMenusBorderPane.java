e630da03d8178b522a3a022dcdf852d97a9054c9
blame110 <46011863+blame110@users.noreply.github.com>
2024-04-09 10:20:49 +0200

    9 4

--- src/main/java/application/EjemploMenusBorderPane.java
+++ src/main/java/application/EjemploMenusBorderPane.java
@@ -3,13 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -28,8 +34,40 @@ public class EjemploMenusBorderPane extends Application {
 		// ZONA SUPERIOR MENU
 		Label lblMenu = new Label("Menu");
 
+		// Barra de menus principal
+		MenuBar barraMenu = new MenuBar();
+
+		// Menus que van en la barra de menus
+		Menu mArchivo = new Menu("Archivo");
+		Menu mOpciones = new Menu("Opciones");
+		Menu mAyuda = new Menu("Ayuda");
+		Menu mHerr = new Menu("Herramientas");
+
+		// Opciones de menu que aparecen cuando
+		// seleccionamos el menu que las contiene
+		MenuItem ICargar = new MenuItem("Cargar");
+		MenuItem IGuardar = new MenuItem("Guardar");
+		MenuItem ISalir = new MenuItem("Salir");
+
+		MenuItem IPreferencias = new MenuItem("Preferencias");
+
+		MenuItem IManual = new MenuItem("Manual Referencia");
+		MenuItem IAcerca = new MenuItem("Acerca de");
+
+		MenuItem ICopiar = new MenuItem("Copiar");
+		MenuItem IPegar = new MenuItem("Pegar");
+
+		// Asignamos los items a los menus
+		mArchivo.getItems().addAll(ICargar, IGuardar, ISalir);
+		mOpciones.getItems().addAll(IPreferencias, mHerr);
+		mAyuda.getItems().addAll(IManual, IAcerca);
+		mHerr.getItems().addAll(ICopiar, IPegar);
+
+		// Añadimos los menus a la barra
+		barraMenu.getMenus().addAll(mArchivo, mOpciones, mAyuda);
+
 		// Asignamos el menu a la parte superior
-		pnlDistribucion.setTop(lblMenu);
+		pnlDistribucion.setTop(barraMenu);
 		pnlDistribucion.setMargin(lblMenu, new Insets(10, 7, 10, 7));
 		pnlDistribucion.setAlignment(lblMenu, Pos.CENTER);
 
@@ -53,7 +91,8 @@ public class EjemploMenusBorderPane extends Application {
 
 		VBox panelCentral = new VBox();
 
-		Label lblValorNota = new Label("5");
+		TextField txtValorNota = new TextField("5");
+		txtValorNota.setMaxWidth(30);
 
 		Slider slider = new Slider();
 		slider.setMaxWidth(380);
@@ -66,29 +105,53 @@ public class EjemploMenusBorderPane extends Application {
 		slider.setMinorTickCount(1);
 		slider.setBlockIncrement(1);
 
+		TextArea textoFich = new TextArea();
+
+		// Añadimos el slider y el textarea al Vbox
+		panelCentral.getChildren().addAll(txtValorNota, slider, textoFich);
+
+		panelCentral.setMargin(slider, new Insets(5, 7, 5, 7));
+		panelCentral.setMargin(textoFich, new Insets(5, 7, 5, 7));
+		panelCentral.setMargin(txtValorNota, new Insets(5, 7, 5, 7));
+
+		// Añadimos el Vbox al centro del borderpane
+		// principal
+		pnlDistribucion.setCenter(panelCentral);
+
+		// GESTION DE EVENTOS
 		// Cuando el slider cambia de valor en lugar de
 		// un EventHandler,
 		// Se utiliza un Listener
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
 
-				lblValorNota.setText(new_val.toString());
+				txtValorNota.setText(String.format("%.2f", new_val));
 
 			}
 		});
 
-		TextArea textoFich = new TextArea();
+		// Cuando modificamos el valor cambiamos la
+		// posicion del slider
+		txtValorNota.setOnAction(new EventHandler<ActionEvent>() {
 
-		// Añadimos el slider y el textarea al Vbox
-		panelCentral.getChildren().addAll(lblValorNota, slider, textoFich);
+			@Override
+			public void handle(ActionEvent event) {
+				// Le asignamos al valor del slider el
+				// numero contenido en el txtvalornota
+				slider.setValue(Double.parseDouble(txtValorNota.getText()));
+			}
+		});
 
-		panelCentral.setMargin(slider, new Insets(5, 7, 5, 7));
-		panelCentral.setMargin(textoFich, new Insets(5, 7, 5, 7));
-		panelCentral.setMargin(lblValorNota, new Insets(5, 7, 5, 7));
+		// Cuando pulsamos en la opcion de menu salir
+		// cerramos la app
+		ISalir.setOnAction(new EventHandler<ActionEvent>() {
 
-		// Añadimos el Vbox al centro del borderpane
-		// principal
-		pnlDistribucion.setCenter(panelCentral);
+			@Override
+			public void handle(ActionEvent event) {
+				// Cerramos la ventana principal
+				stage.close();
+			}
+		});
 
 		// La scene es el contenido de la ventana,
 		// cuando se crea se define su tamaño por
